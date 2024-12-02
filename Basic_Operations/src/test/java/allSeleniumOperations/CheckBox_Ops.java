18866108b3966e3aa8883902cc427db06c507818
bipul475000 <bipul.mahato@outlook.com>
2024-12-02 22:32:17 +0530

    New class and xpath for CheckBox

---
+++ Basic_Operations/src/test/java/allSeleniumOperations/CheckBox_Ops.java
@@ -0,0 +1,39 @@
+package allSeleniumOperations;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import basePackage.BaseTest;
+
+public class CheckBox_Ops extends BaseTest {
+
+	@Test
+	public void selectAndDeselect() throws InterruptedException {
+
+		driver.findElement(By.xpath(properties.getProperty("FashionDropDown"))).click();
+		WebElement fashionBag = driver.findElement(By.xpath(properties.getProperty("FashionDropDown_Bags")));	
+		fashionBag.click();
+		
+		WebElement checkBox = driver.findElement(By.xpath(properties.getProperty("fAssured_CheckBox")));
+//		
+//		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(15));
+//		
+//		wait.until(ExpectedConditions.elementToBeSelected(checkBox));
+	
+		checkBox.click();
+		
+		//Testing
+		
+		System.out.println("testing");
+		
+		
+	}
+}
