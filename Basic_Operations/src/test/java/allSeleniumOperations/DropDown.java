7742ac3484d2a76ace5d530943e1ee77823a25d6
bipul475000 <bipul.mahato@outlook.com>
2024-12-03 21:29:54 +0530

    Parallel and sequential run

--- Basic_Operations/src/test/java/allSeleniumOperations/DropDown.java
+++ Basic_Operations/src/test/java/allSeleniumOperations/DropDown.java
@@ -1,18 +1,21 @@
 package allSeleniumOperations;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import basePackage.BaseTest;
+import basePackage.BaseTestParallel;
 
-public class DropDown extends BaseTest {
+public class DropDown extends BaseTestParallel {
 	
 	@Test
-	public void dropDown() {
+	public void dropDown() {	
+		getDriver().get("https://www.flipkart.com/");
+		getDriver().manage().window().maximize();
+		getDriver().findElement(By.xpath(properties.getProperty("FashionDropDown"))).click();
 		
-		driver.get("https://www.flipkart.com/");
-		driver.manage().window().maximize();
-		driver.findElement(By.xpath(properties.getProperty("FashionDropDown"))).click();
 	}
 
 }
